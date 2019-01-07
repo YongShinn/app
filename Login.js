05b5bb15ab3f8877d4ed924888893e660f5c1f52
JonasDAnderson <jonasdeuce@gmail.com>
2019-01-07 14:00:59 +0800

    added register and login to frontend

--- Login.js
+++ Login.js
@@ -4,10 +4,62 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var jsdom = require("jsdom");
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+const CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
+const AWS = require('aws-sdk');
+const request = require('request');
+const jwkToPem = require ('jwk-to-pem');
+const jwt = require('jsonwebtoken');
+global.fetch = require('node-fetch');
 const { JSDOM } = jsdom;
 const { window } = new JSDOM();
 const { document } = (new JSDOM('')).window;
 global.document = document;
+
+const poolData = {
+  UserPoolId : "ap-southeast-1_npYY55FZJ",
+  ClientId : "5ng046js96l2gcqefbiahjupet"
+};
+const pool_region = 'ap-southeast-1';
+const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+function RegisterUser(userEmail, userPassword){
+  var attributeList = [];
+  attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({Name:"email",Value:userEmail}));
+  userPool.signUp(userEmail, userPassword, attributeList, null, function(err, result){
+      if (err) {
+          console.log(err);
+          return;
+      }
+      cognitoUser = result.user;
+      console.log('user name is ' + cognitoUser.getUsername());
+  }); 
+}
+
+function Login(userEmail, userPassword) {
+  var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+      Username : userEmail,
+      Password : userPassword,
+  });
+
+  var userData = {
+      Username : userEmail,
+      Pool : userPool
+  };
+  var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+
+  cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: function (result) {
+          console.log('access token + ' + result.getAccessToken().getJwtToken());
+          console.log('id token + ' + result.getIdToken().getJwtToken());
+          console.log('refresh token + ' + result.getRefreshToken().getToken());
+      },
+      onFailure: function(err) {
+          console.log(err);
+      },
+
+  });
+}
 
 var $ = require('jquery')(window);
 
@@ -21,19 +73,18 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res){ // sign in
   var uEmail = req.body.uEmail;
   var uPassword = req.body.uPassword;
-  console.log(uEmail+uPassword);
+  Login(uEmail, uPassword);
+  console.log(uEmail);
+  console.log(uPassword);
 });
 
-app.post("/a", function(req, res){
+app.post("/a", function(req, res){ //register
   var mail = req.body.mail;
   var pas = req.body.psw;
-  var add1 = req.body.address1;
-  var add2 = req.body.address2;
-  var postal = req.body.postal;
-  console.log(mail+pas+add1+add2+postal);
+  RegisterUser(mail, pas);
 });
 
 app.listen(3000, function() {
